e82b0d7ad6c97482b2570b3a21cebd4291bcfbd0
jessie c :) <jcaibmi2@gmail.com>
2026-09-22 08:59:18 -0700

    (GP-2.1): Program Can Initialize Repository

--- Git.java
+++ Git.java
@@ -1,5 +1,30 @@
+import java.io.*;
+
 public class Git {
     public static void main(String[] args) {
-        
+        Git.init();
+    }
+
+    public static void init () {
+        File gitDir = new File("./git/");
+        if (!gitDir.mkdir()) {
+            System.out.println("Git Repository Already Exists");
+        }
+
+        File objDir = new File("./git/objects/");
+        File index = new File("./git/index");
+        File head = new File ("./git/HEAD");
+
+        try {
+            if (!objDir.mkdir() || !index.createNewFile() || !head.createNewFile()) {
+                System.out.println("Git Repository Already Exists");
+                return;
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to initialize Git repo");
+            return;
+        }
+
+        System.out.println("Git Repository Created");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # i-forgit
 i forgor anyways we're making git or something for honors topics
+
+init(): initialize's the Git repository
