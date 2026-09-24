b91d688c3e945cb417e5054ccc63f85d20212309
jessie c :) <jcaibmi2@gmail.com>
2026-09-24 14:16:20 -0700

    (GP-2.2): Created a Hash Function
    - Passes in the filePath of the file to be hashed
    - Reads contents of file into a StringBuilder
    - Turns the contents into bytes
    - Hashes bytes with SHA-1
    - Returns hashed file contents

--- Git.java
+++ Git.java
@@ -1,8 +1,11 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
 
 public class Git {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Git.init();
+        System.out.println(Git.hashFile("test.txt"));
     }
 
     public static void init () {
@@ -26,5 +29,37 @@ public class Git {
         }
 
         System.out.println("Git Repository Created");
+    }
+
+    public static String hashFile(String filePath) throws IOException {
+        StringBuilder stuffInFile = new StringBuilder();
+        StringBuilder hexString = new StringBuilder();
+
+        // gets stuffInFile
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                stuffInFile.append(line);
+            }
+        }
+        
+        // turn stuffInFile to bytes
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] bytes = md.digest(stuffInFile.toString().getBytes(StandardCharsets.UTF_8));
+
+            // bytes to hash
+            for (byte b : bytes) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("NoSuchAlgorithmException: file couldn't be hashed :(");
+        }
+
+        return hexString.toString();
+
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # i-forgit
 i forgor anyways we're making git or something for honors topics
 
-init(): initialize's the Git repository
+## init()
+initialize's the Git repository
+
+## hashFile(String filePath)
+Reads contents of file into a StringBuilder
+Turns the contents into bytes
+Hashes bytes with SHA-1
+Returns hashed file contents
+
+
